ad05c2fe1408306cfd738889b01763435ce74da1
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-09-01 15:41:42 -0500

    classes 8-12

---
+++ 01-dart-intro/11_futures.dart
@@ -0,0 +1,18 @@
+void main(){
+  print('inicio del programa');
+  
+  httpGet('https://fernando-herrera.com/cursos').then((value){
+    print(value);
+  }).catchError((err){
+    print('Error: $err');
+  });
+  
+  print('fin del programa');
+}
+
+Future<String> httpGet(String url){
+  return Future.delayed(const Duration(seconds: 1), (){
+    throw 'Error en la peticion http';
+    //return 'Respuesta de la peticion http';
+  });
+}
