6dd33793fdb65b2f8ad4d6f3bbf6c834050f5d1a
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-09-21 10:42:40 -0500

    Messages bubbles

---
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_burble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://image.enjoymovie.net/tloPmJfugquyioMHHb0ZfQqEOlA=/256x256/smart/core/p/mMWMMg0qWv.jpg'),
+          ),
+        ),
+        title: const Text('Mi amor ❤️'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return (index % 2 == 0)
+                          ? const HerMessageBurble()
+                          : const MyMessageBubble();
+                    })),
+            const Text('hola'),
+          ],
+        ),
+      ),
+    );
+  }
+}
