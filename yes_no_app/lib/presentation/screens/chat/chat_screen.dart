7c6ea6c35d191a8c2b0248bc2b7ff1342241a16b
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-10-05 15:58:58 -0500

    Section 6 finished

--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -40,13 +40,13 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
-                  controller: chatProvider.chatScrollController,
+                    controller: chatProvider.chatScrollController,
                     itemCount: chatProvider.messageList.length,
                     itemBuilder: (context, index) {
                       final message = chatProvider.messageList[index];
 
                       return (message.fromWho == FromWho.hers)
-                          ? const HerMessageBurble()
+                          ? HerMessageBurble(message: message)
                           : MyMessageBubble(message: message);
                     })),
 
