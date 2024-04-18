2058af8852085eed64afa5c8b4ef33c229826691
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-04-18 09:39:34 -0500

    tutorial screen link added

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -44,4 +44,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Una serie de controles de Flutter',
       link: '/ui-controls',
       icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: 'Introducción a la app',
+      subTitle: 'Tutorial introductorio',
+      link: '/tutorial',
+      icon: Icons.accessible_rounded),
 ];
