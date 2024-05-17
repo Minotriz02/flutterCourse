be56b01607d3fc8efd606ce73d041a3854ea4f5a
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-05-17 10:33:04 -0500

    formz validates all fields

--- forms_app/lib/infrastructure/inputs/password.dart
+++ forms_app/lib/infrastructure/inputs/password.dart
@@ -11,6 +11,17 @@ class Password extends FormzInput<String, PasswordError> {
   // Call super.dirty to represent a modified form input.
   const Password.dirty(String value) : super.dirty(value);
 
+    String? get errorMessage {
+    if (isValid || isPure) return null;
+    if (displayError == PasswordError.empty) {
+      return 'La contraseña no puede estar vacía';
+    }
+    if (displayError == PasswordError.length) {
+      return 'La contraseña de usuario debe tener al menos 6 caracteres';
+    }
+    return null;
+  }
+
   // Override validator to handle validating a given input value.
   @override
   PasswordError? validator(String value) {
--- forms_app/lib/presentation/blocs/register/register_cubit.dart
+++ forms_app/lib/presentation/blocs/register/register_cubit.dart
@@ -9,26 +9,48 @@ class RegisterCubit extends Cubit<RegisterFormState> {
   RegisterCubit() : super(const RegisterFormState());
 
   void onSubmit() {
-    print('Submit $state');
+    emit(state.copyWith(
+      formStatus: FormStatus.validating,
+      username: Username.dirty(state.username.value),
+      password: Password.dirty(state.password.value),
+      email: Email.dirty(state.email.value),
+      isValid: Formz.validate([state.username, state.password]),
+    ));
   }
 
   void usernameChanged(String value) {
     final username = Username.dirty(value);
     emit(state.copyWith(
       username: username,
-      isValid: Formz.validate([username, state.password]),
+      isValid: Formz.validate([
+        username,
+        state.password,
+        state.email,
+      ]),
     ));
   }
 
   void emailChanged(String value) {
-    emit(state.copyWith(email: value));
+    final email = Email.dirty(value);
+    emit(state.copyWith(
+      email: email,
+      isValid: Formz.validate([
+        state.username,
+        state.password,
+        email,
+      ]),
+    ));
   }
 
   void passwordChanged(String value) {
     final password = Password.dirty(value);
     emit(state.copyWith(
       password: password,
-      isValid: Formz.validate([state.username, password]),
+      isValid: Formz.validate([
+        state.username,
+        password,
+        state.email,
+      ]),
     ));
   }
 }
