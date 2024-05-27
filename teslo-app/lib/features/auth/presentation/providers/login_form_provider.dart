fb394c506d254abbb22fe01502da18d5e96028a7
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-05-27 10:10:46 -0500

    dot env configured

--- teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -2,6 +2,52 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
+final loginFormProvider =
+    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>(
+  (ref) {
+    return LoginFormNotifier();
+  },
+);
+
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  LoginFormNotifier() : super(LoginFormState());
+
+  onEmailChange(String value) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([newEmail, state.password]),
+    );
+  }
+
+  onPasswordChange(String value) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([newPassword, state.email]),
+    );
+  }
+
+  onFormSubmit() {
+    _touchEveryField();
+    if (!state.isValid) return;
+
+    print(state);
+  }
+
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+    );
+  }
+}
+
 class LoginFormState {
   final bool isPosting;
   final bool isFormPosted;
@@ -44,49 +90,3 @@ class LoginFormState {
 ''';
   }
 }
-
-class LoginFormNotifier extends StateNotifier<LoginFormState> {
-  LoginFormNotifier() : super(LoginFormState());
-
-  onEmailChange(String value) {
-    final newEmail = Email.dirty(value);
-    state = state.copyWith(
-      email: newEmail,
-      isValid: Formz.validate([newEmail, state.password]),
-    );
-  }
-
-  onPasswordChange(String value) {
-    final newPassword = Password.dirty(value);
-    state = state.copyWith(
-      password: newPassword,
-      isValid: Formz.validate([newPassword, state.email]),
-    );
-  }
-
-  onFormSubmit() {
-    _touchEveryField();
-    if (!state.isValid) return;
-
-    print(state);
-  }
-
-  _touchEveryField() {
-    final email = Email.dirty(state.email.value);
-    final password = Password.dirty(state.password.value);
-
-    state = state.copyWith(
-      isFormPosted: true,
-      email: email,
-      password: password,
-      isValid: Formz.validate([email, password]),
-    );
-  }
-}
-
-final loginFormProvider =
-    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>(
-  (ref) {
-    return LoginFormNotifier();
-  },
-);
--- teslo-app/lib/main.dart
+++ teslo-app/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/config/config.dart';
-import 'package:teslo_shop/config/router/app_router.dart';
 
-void main() {
-  runApp(ProviderScope(child: MainApp()));
+void main() async {
+  await Environment.initEnvironment();
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
