5cfb3335c13ac0516319cf13af0376aeffe4ccc8
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-05-28 10:06:25 -0500

    error message in login showed

--- teslo-app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ teslo-app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -25,13 +25,16 @@ class AuthDataSourceImpl extends AuthDataSource {
       final user = UserMapper.userJsonToEntity(response.data);
       return user;
     } on DioException catch (e) {
-      if (e.response?.statusCode == 401) throw WrongCredentials();
-      if (e.type == DioExceptionType.connectionTimeout) {
-        throw ConnectionTimeout();
+      if (e.response?.statusCode == 401) {
+        throw CustomError(
+            e.response?.data['message'] ?? 'Credenciales incorrectas');
       }
-      throw CustomError('Something wrong happened', 1);
+      if (e.type == DioExceptionType.connectionTimeout) {
+        throw CustomError('Revisar conexcion a internet');
+      }
+      throw Exception();
     } catch (e) {
-      throw CustomError('Something wrong happened', 2);
+      throw Exception();
     }
   }
 
