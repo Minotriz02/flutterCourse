5f2024d47cb69123f0a267d27ff0451ea6435804
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-05-28 09:39:41 -0500

    provider auth started and error implemented

--- teslo-app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ teslo-app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -24,8 +24,14 @@ class AuthDataSourceImpl extends AuthDataSource {
 
       final user = UserMapper.userJsonToEntity(response.data);
       return user;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) throw WrongCredentials();
+      if (e.type == DioExceptionType.connectionTimeout) {
+        throw ConnectionTimeout();
+      }
+      throw CustomError('Something wrong happened', 1);
     } catch (e) {
-      throw WrongCredentials();
+      throw CustomError('Something wrong happened', 2);
     }
   }
 
