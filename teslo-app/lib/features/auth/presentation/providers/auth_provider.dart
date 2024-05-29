632f8538e2c7c39502d1b63f286e28a0bbd5e5f1
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-05-29 09:45:35 -0500

    shared preferences impl to save token

--- teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
+++ teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,17 +1,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = AuthRepositoryImpl();
+  final keyValueStorageService = KeyValueStorageServiceImpl();
 
-  return AuthNotifier(authRepository: authRepository);
+  return AuthNotifier(
+    authRepository: authRepository,
+    keyValueStorageService: keyValueStorageService,
+  );
 });
 
 class AuthNotifier extends StateNotifier<AuthState> {
   final AuthRepository authRepository;
+  final KeyValueStorageService keyValueStorageService;
 
-  AuthNotifier({required this.authRepository}) : super(AuthState());
+  AuthNotifier({
+    required this.authRepository,
+    required this.keyValueStorageService,
+  }) : super(AuthState());
 
   Future<void> loginUser(String email, String password) async {
     await Future.delayed(const Duration(milliseconds: 500));
@@ -28,14 +38,17 @@ class AuthNotifier extends StateNotifier<AuthState> {
   void registerUser(String email, String password) async {}
   void checkAuthStatus() async {}
 
-  _setLoggedUser(User user) {
+  _setLoggedUser(User user) async {
+    await keyValueStorageService.setKeyValue('token', user.token);
     state = state.copyWith(
       authStatus: AuthStatus.authenticated,
       user: user,
+      errorMessage: '',
     );
   }
 
   Future<void> logout([String? errorMessage]) async {
+    await keyValueStorageService.removeKey('token');
     state = state.copyWith(
       authStatus: AuthStatus.notAuthenticated,
       user: null,
---
+++ teslo-app/lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
@@ -0,0 +1,42 @@
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class KeyValueStorageServiceImpl extends KeyValueStorageService {
+  Future<SharedPreferences> getSharedPrefs() async {
+    return await SharedPreferences.getInstance();
+  }
+
+  @override
+  Future<T?> getValue<T>(String key) async {
+    final prefs = await getSharedPrefs();
+    switch (T) {
+      case String:
+        return prefs.getString(key) as T?;
+      case int:
+        return prefs.getInt(key) as T?;
+      default:
+        throw UnimplementedError('Type not supported: ${T.runtimeType}');
+    }
+  }
+
+  @override
+  Future<bool> removeKey(String key) async {
+    final prefs = await getSharedPrefs();
+    return await prefs.remove(key);
+  }
+
+  @override
+  Future<void> setKeyValue<T>(String key, T value) async {
+    final prefs = await getSharedPrefs();
+    switch (T) {
+      case String:
+        prefs.setString(key, value as String);
+        break;
+      case int:
+        prefs.setInt(key, value as int);
+        break;
+      default:
+        throw UnimplementedError('Type not supported: ${T.runtimeType}');
+    }
+  }
+}
