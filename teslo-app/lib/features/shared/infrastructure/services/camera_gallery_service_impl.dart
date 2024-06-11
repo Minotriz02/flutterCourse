171a01f968c5fadbc33494f04d68ed6fa7a04a5d
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-06-11 09:05:49 -0500

    formatted

--- teslo-app/lib/features/shared/infrastructure/services/camera_gallery_service_impl.dart
+++ teslo-app/lib/features/shared/infrastructure/services/camera_gallery_service_impl.dart
@@ -3,39 +3,33 @@ import 'package:image_picker/image_picker.dart';
 import 'camera_gallery_service.dart';
 
 class CameraGalleryServiceImpl extends CameraGalleryService {
-
   final ImagePicker _picker = ImagePicker();
 
   @override
   Future<String?> selectPhoto() async {
-     final XFile? photo = await _picker.pickImage(
+    final XFile? photo = await _picker.pickImage(
       source: ImageSource.gallery,
       imageQuality: 80,
     );
 
-    if ( photo == null ) return null;
+    if (photo == null) return null;
 
-    print('Tenemos una imagen ${ photo.path }');
+    print('Tenemos una imagen ${photo.path}');
 
     return photo.path;
   }
 
   @override
   Future<String?> takePhoto() async {
-    
     final XFile? photo = await _picker.pickImage(
-      source: ImageSource.camera,
-      imageQuality: 80,
-      preferredCameraDevice: CameraDevice.rear
-    );
+        source: ImageSource.camera,
+        imageQuality: 80,
+        preferredCameraDevice: CameraDevice.rear);
 
-    if ( photo == null ) return null;
+    if (photo == null) return null;
 
-    print('Tenemos una imagen ${ photo.path }');
+    print('Tenemos una imagen ${photo.path}');
 
     return photo.path;
   }
-
-
-
 }
